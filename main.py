bf900dfb4d5d702cb6a5d1d4dea03c1dbd0fa372
ksg-dev <ksg.dev.data@gmail.com>
2024-06-03 16:57:45 -0500

    guess state logic

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import turtle
+import pandas as pd
+
+ALIGNMENT = "center"
+FONT = ("Calibri", 12, "normal")
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -8,15 +12,31 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-# Create function to get x,y from screen on click
-def get_mouse_click_coor(x, y):
-    print(x,y)
+df = pd.read_csv("50_states.csv")
+states = df["state"]
+# print(type(states))
 
+correct_states = 0
 
-turtle.onscreenclick(get_mouse_click_coor)
+while correct_states == 0:
+    choice = screen.textinput(title="Guess the State", prompt="Name a state!").title().strip()
+    for state in states:
 
-# Keep screen open while clicking
-turtle.mainloop()
+        if choice == state:
+            print(f"Correct: {choice}")
+            label_x = df[df['state'] == state]['x']
+            print(label_x)
+            # label = turtle.Turtle()
+            # label.penup()
+            # label.hideturtle()
+            # label.goto(x=df["x"], y=df["y"])
+            # state.write(df["state"], align=ALIGNMENT, font=FONT)
+            # correct_states += 1
+
+    #print(f"States: {states}")
+
+# while 0 < correct_states < 50:
+#     answer = screen.textinput(title="")
 
 
 
