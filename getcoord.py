f70ce38c083f366ab49275157371745d9c97066e
ksg-dev <ksg.dev.data@gmail.com>
2024-06-03 16:13:15 -0500

    find coordinates on turtle screen

--- getcoord.py
+++ getcoord.py
@@ -3,16 +3,19 @@ import turtle
 screen = turtle.Screen()
 screen.title("U.S States Game")
 
+# path to image
 image = "blank_states_img.gif"
 screen.addshape(image)
 
+# Create turtle w screen shape
 turtle.shape(image)
 
-# Create function to get x,y from screen on click
+
+# Create function to get x,y from screen on click so you know where states are
 def get_mouse_click_coor(x, y):
-    print(x,y)
+    print(x, y)
 
-
+# tell turtle to record coordinates when clicked
 turtle.onscreenclick(get_mouse_click_coor)
 
 # Keep screen open while clicking
